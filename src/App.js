472db854b33229be4c25f15e665211b1af4df44c
itc-s21002 <s21002@std.it-college.ac.jp>
2021-11-25 07:58:18 +0000

    pokemon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 
 const App = () => {
-  return <h1>hello</h1>
+  const [date, setDate] = useState([])
+  const uri = 'https://pokeapi.co/api/v2/pokemon/'
+  useEffect(() => (
+    window.fetch(uri)
+      .then(res => res.json())
+      .then(json => json.results)
+      .then(date => setDate(date))
+  ), [])
+
+  return (
+    <>
+      <ul>
+        {date.map(v => (
+          <li key={v.url}>{v.name}</li>
+        ))}
+      </ul>
+    </>
+  )
 }
 export default App
