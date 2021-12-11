527f70ee90489e49c4c60340d2d3841dc97d992c
itc-s21002 <s21002@std.it-college.ac.jp>
2021-12-11 20:05:31 +0000

    poke途中

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
 
+const values = [{ id: 1, item: 'uri1' }]
+const SelectItems = values.map(value => {
+  return (
+    <option value={value.item} Key={value.id}>
+      {value.item}
+    </option>
+  )
+})
 const App = () => {
-  const [date, setDate] = useState([])
+  const [selectedValue, setSelectedValue] = useState(values[0].item)
+  const handleChange = e => {
+    setSelectValue(e.target.value)
+  }
   const uri = 'https://pokeapi.co/api/v2/pokemon/'
   useEffect(
     () =>
@@ -15,13 +26,20 @@ const App = () => {
   )
 
   return (
-    <>
+    <div className='App'>
       <ul>
+        <p>
+          現在選択されている値:
+          <b>{selectedValue}</b>
+        </p>
+        <select value={selectValue} onChange={handleChange}>
+          {SelectItems}
+        </select>
         {date.map(v => (
           <li key={v.url}>{v.name}</li>
         ))}
       </ul>
-    </>
+    </div>
   )
 }
 export default App
