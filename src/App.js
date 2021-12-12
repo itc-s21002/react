8b521a24a653cfb9a3983c06475547c78c1fe580
itc-s21002 <s21002@std.it-college.ac.jp>
2021-12-12 14:30:56 +0000

    eiga

--- src/App.js
+++ src/App.js
@@ -1,45 +1,64 @@
 import React, { useState, useEffect } from 'react'
-import './App.css'
+import "./App.css"
+import Header from "./Header"
+import Movie from "./Movie";
+import Search from "./Search";
 
-const values = [{ id: 1, item: 'uri1' }]
-const SelectItems = values.map(value => {
-  return (
-    <option value={value.item} Key={value.id}>
-      {value.item}
-    </option>
-  )
-})
+
+const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=4a3b711b";
+
+
 const App = () => {
-  const [selectedValue, setSelectedValue] = useState(values[0].item)
-  const handleChange = e => {
-    setSelectValue(e.target.value)
-  }
-  const uri = 'https://pokeapi.co/api/v2/pokemon/'
-  useEffect(
-    () =>
-      window
-        .fetch(uri)
-        .then(res => res.json())
-        .then(json => json.results)
-        .then(date => setDate(date)),
-    []
-  )
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
 
-  return (
-    <div className='App'>
-      <ul>
-        <p>
-          現在選択されている値:
-          <b>{selectedValue}</b>
-        </p>
-        <select value={selectValue} onChange={handleChange}>
-          {SelectItems}
-        </select>
-        {date.map(v => (
-          <li key={v.url}>{v.name}</li>
-        ))}
-      </ul>
+  useEffect(() => {
+    fetch(MOVIE_API_URL)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        setMovies(jsonResponse.Search);
+        setLoading(false);
+      });
+  }, []);
+
+  const search = searchValue => {
+    setLoading(true);
+    setErrorMessage(null);
+
+    fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        if (jsonResponse.Response === "True") {
+          setMovies(jsonResponse.Search);
+          setLoading(false);
+        } else {
+          setErrorMessage(jsonResponse.Error);
+          setLoading(false);
+        }
+      });
+    };
+
+
+    return (
+     <div className="App">
+      <Header text="HOOKED" />
+      <Search search={search} />
+      <p className="App-intro">Sharing a few of our favourite movies</p>
+      <div className="movies">
+        {loading && !errorMessage ? (
+         <span>loading...</span>
+         ) : errorMessage ? (
+          <div className="errorMessage">{errorMessage}</div>
+        ) : (
+          movies.map((movie, index) => (
+            <Movie key={`${index}-${movie.Title}`} movie={movie} />
+          ))
+        )}
+      </div>
     </div>
-  )
-}
-export default App
+  );
+};
+
+
+export default App;
